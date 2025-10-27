558bd1213a35864e064ea6e6224b1ac9aef76193
parcidio <parcidioandre@gmail.com>
2025-10-28 00:41:21 +0100

    Workspace deletion

--- client/src/components/workspace/settings/delete-workspace-card.tsx
+++ client/src/components/workspace/settings/delete-workspace-card.tsx
@@ -1,15 +1,52 @@
 import { ConfirmDialog } from "@/components/resuable/confirm-dialog";
+import PermissionsGuard from "@/components/resuable/permission-guard";
 import { Button } from "@/components/ui/button";
+import { Permissions } from "@/constant";
+import { useAuthContext } from "@/context/auth-provider";
 import useConfirmDialog from "@/hooks/use-confirm-dialog";
+import { toast } from "@/hooks/use-toast";
+import useWorkspaceId from "@/hooks/use-workspace-id";
+import { deleteWorkspaceMutationFn } from "@/lib/api";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
 
 const DeleteWorkspaceCard = () => {
+  const { workspace } = useAuthContext();
+  const navigate = useNavigate();
+
+  const queryClient = useQueryClient();
+  const workspaceId = useWorkspaceId();
+
   const { open, onOpenDialog, onCloseDialog } = useConfirmDialog();
 
-  const isPending = false;
+  const { mutate, isPending } = useMutation({
+    mutationFn: deleteWorkspaceMutationFn,
+  });
 
-  const handleConfirm = () => {};
+  const handleConfirm = () => {
+    mutate(workspaceId, {
+      onSuccess: (data) => {
+        queryClient.invalidateQueries({
+          queryKey: ["userWorkspaces"],
+        });
+        navigate(`/workspace/${data.currentWorkspace}`);
+        setTimeout(() => onCloseDialog(), 100);
+      },
+      onError: (error) => {
+        toast({
+          title: "Error",
+          description: error.message,
+          variant: "destructive",
+        });
+      },
+    });
+  };
   return (
     <>
+      {/*<PermissionsGuard
+          showMessage
+          requiredPermission={Permissions.DELETE_WORKSPACE}
+        >*/}
       <div className="w-full">
         <div className="mb-5 border-b">
           <h1
@@ -20,23 +57,25 @@ const DeleteWorkspaceCard = () => {
           </h1>
         </div>
 
-        <div className="flex flex-col items-start justify-between py-0">
-          <div className="flex-1 mb-2">
-            <p>
-              Deleting a workspace is a permanent action and cannot be undone.
-              Once you delete a workspace, all its associated data, including
-              projects, tasks, and member roles, will be permanently removed.
-              Please proceed with caution and ensure this action is intentional.
-            </p>
+      
+          <div className="flex flex-col items-start justify-between py-0">
+            <div className="flex-1 mb-2">
+              <p>
+                Deleting a workspace is a permanent action and cannot be undone.
+                Once you delete a workspace, all its associated data, including
+                projects, tasks, and member roles, will be permanently removed.
+                Please proceed with caution and ensure this action is
+                intentional.
+              </p>
+            </div>
+            <Button
+              className="shrink-0 flex place-self-end h-[40px]"
+              variant="destructive"
+              onClick={onOpenDialog}
+            >
+              Delete Workspace
+            </Button>
           </div>
-          <Button
-            className="shrink-0 flex place-self-end h-[40px]"
-            variant="destructive"
-            onClick={onOpenDialog}
-          >
-            Delete Workspace
-          </Button>
-        </div>
       </div>
 
       <ConfirmDialog
@@ -44,11 +83,13 @@ const DeleteWorkspaceCard = () => {
         isLoading={isPending}
         onClose={onCloseDialog}
         onConfirm={handleConfirm}
-        title={`Delete  Test co Workspace`}
+        title={`Delete  ${workspace?.name} Workspace`}
         description={`Are you sure you want to delete? This action cannot be undone.`}
         confirmText="Delete"
         cancelText="Cancel"
       />
+        {/*</PermissionsGuard>*/}
+
     </>
   );
 };
